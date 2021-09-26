190ea66a7778c625c0d7351c895938656c7a1ba4
Daniil <daniil@MacBook-Air-Daniil.local>
2021-09-26 14:01:23 +0300

    [v.0.1.0-beta.2] Some fixes

--- main.js
+++ main.js
@@ -1,103 +1,108 @@
 
-const createEventBus = () => {
-	try {
-		if(typeof process !== 'undefined' && null !== process.versions && null !== process.versions.node){
-			const EventEmitter = require('events');
-			return [
-				new EventEmitter(),
-				true,
-			];
+module.exports = (() => {
+	const typeof_undefined = 'undefined';
+	const [
+		is_emitter_nodejs,
+		is_emitter_eventtarget,
+		is_emitter_element,
+		is_emitter_self,
+		is_event_name_need_prefix,
+	] = (() => {
+		if(typeof process !== typeof_undefined && null !== process.versions && null !== process.versions.node){
+			return [ true ];
 		}
-	}
-	catch{}
+		if(typeof EventTarget !== typeof_undefined){
+			return [ false, true ];
+		}
+		if(typeof document !== typeof_undefined && typeof document.createElement === 'function'){
+			return [ false, false, true ];
+		}
+		if(typeof self !== typeof_undefined){
+			return [ false, false, false, true, true ];
+		}
+		throw new Error('Cannot create event listener. Check your environment');
+	})();
 
-	try {
-		return [
-			new EventTarget(),
-		];
-	}
-	catch{}
+	const createEventBus = () => {
+		if(is_emitter_nodejs){
+			const EventEmitter = require('events');
+			return new EventEmitter();
+		}
 
-	try {
-		return [
-			document.createElement('p'),
-		];
-	}
-	catch{}
+		if(is_emitter_eventtarget){
+			return new EventTarget();
+		}
 
-	try {
-		return [
-			self,
-			false,
-			true,
-		];
-	}
-	catch{}
+		if(is_emitter_element){
+			return document.createElement('p');
+		}
 
-	throw new Error('Cannot create event listener. Check your environment');
-};
+		if(is_emitter_self){
+			return self;
+		}
+	};
 
-const EventEmitter = module.exports = function EventEmitter () {
-	const [ bus, is_nodejs_emitter, need_prefix ] = createEventBus();
+	const EventEmitter = function EventEmitter () {
+		this._bus = createEventBus();
+		this._event_name_prefix = is_event_name_need_prefix ? `eventemitter-${Math.random()}-` : '';
+	};
 
-	this._bus = bus;
-	this._is_nodejs_emitter = true === is_nodejs_emitter;
-	this._event_name_prefix = (true === need_prefix) ? `eventemitter-${Math.random()}-` : '';
-};
+	EventEmitter.prototype.on = function (event_name, listener) {
+		event_name = this._event_name_prefix + event_name;
 
-EventEmitter.prototype.on = function (event_name, listener) {
-	event_name = this._event_name_prefix + event_name;
+		const bus = this._bus;
 
-	const bus = this._bus;
+		if(is_emitter_nodejs){
+			bus.on(event_name, listener);
+			return () => {
+				bus.off(event_name, listener);
+			};
+		}
+		else {
+			const listener_this = (event) => {
+				listener(...event.detail);
+			};
 
-	if(this._is_nodejs_emitter){
-		bus.on(event_name, listener);
-		return () => {
-			bus.off(event_name, listener);
-		};
-	}
-	else {
-		const listener_this = (event) => {
-			listener(...event.detail);
-		};
-
-		bus.addEventListener(event_name, listener_this);
-		return () => {
-			bus.removeEventListener(event_name, listener_this);
-		};
-	}
-};
-EventEmitter.prototype.once = function (event_name, listener) {
-	const off = this.on(
-		event_name,
-		(...args) => {
-			off();
-			listener(...args);
-		},
-	);
-	return off;
-};
-EventEmitter.prototype.emit = function (event_name, ...args) {
-	event_name = this._event_name_prefix + event_name;
-
-	const bus = this._bus;
-
-	if(this._is_nodejs_emitter){
-		bus.emit(
+			bus.addEventListener(event_name, listener_this);
+			return () => {
+				bus.removeEventListener(event_name, listener_this);
+			};
+		}
+	};
+	EventEmitter.prototype.once = function (event_name, listener) {
+		const off = this.on(
 			event_name,
-			...args,
+			(...args) => {
+				off();
+				listener(...args);
+			},
 		);
-	}
-	else {
-		bus.dispatchEvent(
-			new CustomEvent(
+		return off;
+	};
+	EventEmitter.prototype.emit = function (event_name, ...args) {
+		event_name = this._event_name_prefix + event_name;
+
+		const bus = this._bus;
+
+		if(is_emitter_nodejs){
+			bus.emit(
 				event_name,
-				{
-					bubbles: false,
-					cancelable: false,
-					detail: args,
-				},
-			),
-		);
-	}
-};
+				...args,
+			);
+		}
+		else {
+			bus.dispatchEvent(
+				new CustomEvent(
+					event_name,
+					{
+						bubbles: false,
+						cancelable: false,
+						detail: args,
+					},
+				),
+			);
+		}
+	};
+
+	return EventEmitter;
+})();
